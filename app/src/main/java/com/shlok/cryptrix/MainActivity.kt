8110b0c354427b793b5afe0553d103c97541b971
Shlok Gupta <guptashlok41@gmail.com>
2026-08-06 00:37:02 +0530

    Improve UI with Material Design and Snackbar validation

--- app/src/main/java/com/shlok/cryptrix/MainActivity.kt
+++ app/src/main/java/com/shlok/cryptrix/MainActivity.kt
@@ -8,6 +8,8 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.shlok.cryptrix.crypto.CryptoManager
+import com.google.android.material.snackbar.Snackbar
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,19 +28,20 @@ class MainActivity : AppCompatActivity() {
         encryptButton.setOnClickListener {
             val msg = inputMessage.text.toString()
             if(msg.isBlank()){
-                Toast.makeText(this,"Enter Message",Toast.LENGTH_SHORT).show()
+                Snackbar.make(findViewById(android.R.id.content),"Please enter a message",Snackbar.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
             val password = inputKey.text.toString()
             if(password.isBlank()){
-                Toast.makeText(this,"Enter Password",Toast.LENGTH_SHORT).show()
+                Snackbar.make(findViewById(android.R.id.content),"Please enter a password",Snackbar.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
 
             try{
                 outputText.text=cryptoManager.encrypt(msg,password)
+                Snackbar.make(findViewById(android.R.id.content),"Message encrypted successfully ✅",Snackbar.LENGTH_SHORT).show()
             }catch (e:Exception){
-                Toast.makeText(this,"Encryption Failed",Toast.LENGTH_SHORT).show()
+                Snackbar.make(findViewById(android.R.id.content),"Encryption failed",Snackbar.LENGTH_SHORT).show()
             }
         }
 
@@ -55,6 +58,7 @@ class MainActivity : AppCompatActivity() {
             }
             try{
                 outputText.text=cryptoManager.decrypt(msg,password)
+                Snackbar.make(findViewById(android.R.id.content),"Message decrypted successfully ✅",Snackbar.LENGTH_SHORT).show()
             }catch (e:Exception){
                 Toast.makeText(this,"Wrong Password or Invalid Encrypted Text",Toast.LENGTH_SHORT).show()
             }
@@ -64,7 +68,7 @@ class MainActivity : AppCompatActivity() {
             val clipboard = getSystemService(CLIPBOARD_SERVICE) as ClipboardManager
             val clip = ClipData.newPlainText("text", outputText.text)
             clipboard.setPrimaryClip(clip)
-            Toast.makeText(this, "Copied to clipboard!", Toast.LENGTH_SHORT).show()
+            Snackbar.make(findViewById(android.R.id.content),"Copied to clipboard \uD83D\uDCCB",Snackbar.LENGTH_SHORT).show()
         }
     }
 
